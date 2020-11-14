c5bc577846b0e79acc7c07cffb7f78c86abc3cc0
Benjamin Miramontes <benjamin@miramontes.dev>
2020-11-14 11:18:14 -0500

    Implemented opal/crypto, added debug build task

---
+++ include/opal/crypto.h
@@ -0,0 +1,145 @@
+#ifndef _OPAL_CRYPTO_H
+#define _OPAL_CRYPTO_H
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <nacl/crypto_box.h>
+#include <nacl/crypto_secretbox.h>
+
+#define CRYPTO_PACKET_MAX       4096
+#define CRYPTO_LENGTH_BYTES     2
+#define CRYPTO_HEADER_SIZE      (crypto_secretbox_NONCEBYTES + CRYPTO_LENGTH_BYTES)
+
+
+typedef uint8_t local_key_t[crypto_box_SECRETKEYBYTES];
+typedef uint8_t remote_key_t[crypto_box_PUBLICKEYBYTES];
+
+typedef enum crypto_operation_e {
+    NO_OP,
+    WRITE_OP,
+    READ_OP,
+} crypto_operation_e;
+
+typedef enum crypto_channel_status_e {
+    CHANNEL_SUCCESS = 0,
+    CHANNEL_ERROR = -1,
+    CHANNEL_READ_WAIT = -2,
+    CHANNEL_WRITE_WAIT = -3,
+} crypto_channel_status_e;
+
+typedef struct crypto_channel_t {
+    // Underlying fd
+    int fd;
+    // Current operation, for nonblocking channels
+    crypto_operation_e operation;
+    // Key material
+    uint8_t key[crypto_box_BEFORENMBYTES];
+    // Plaintext Buffer
+    struct {
+        uint8_t plaintext_zeroes[crypto_box_ZEROBYTES];
+        union {
+            uint8_t plaintext[0];
+            uint8_t plaintext_data[CRYPTO_PACKET_MAX];
+        };
+    } __attribute__((packed));
+    size_t plaintext_length;
+    // Ciphertext Buffer
+    struct {
+        union {
+#if crypto_box_NONCEBYTES + CRYPTO_LENGTH_BYTES > crypto_box_BOXZEROBYTES
+            struct {
+                union {
+                    uint8_t ciphertext[0];
+                    uint16_t ciphertext_network_length;
+                };
+                uint8_t ciphertext_nonce[crypto_box_NONCEBYTES];
+            } __attribute__((packed));
+            struct {
+                uint8_t ciphertext_padding[(crypto_box_NONCEBYTES + CRYPTO_LENGTH_BYTES) - crypto_box_BOXZEROBYTES];
+                uint8_t ciphertext_zeroes[crypto_box_BOXZEROBYTES];
+            } __attribute__((packed));
+#else
+            uint8_t ciphertext_zeroes[crypto_box_BOXZEROBYTES];
+            struct {
+                uint8_t ciphertext_padding[crypto_box_BOXZEROBYTES - (crypto_box_NONCEBYTES + CRYPTO_LENGTH_BYTES)];
+                struct {
+                    union {
+                        uint8_t ciphertext[0];
+                        uint16_t ciphertext_network_length;
+                    };
+                    uint8_t ciphertext_nonce[crypto_box_NONCEBYTES];
+                } __attribute__((packed));
+            } __attribute__((packed));
+#endif
+        };
+        uint8_t ciphertext_data[CRYPTO_PACKET_MAX];
+    } __attribute__((packed));
+    size_t ciphertext_length;
+    size_t ciphertext_processed;
+    // Leftover data from an incoming chunk, waiting to be consumed
+    // with crypto_channel_read()
+    uint8_t unread_data[CRYPTO_PACKET_MAX];
+    size_t unread_data_start;
+    size_t unread_data_end;
+    // Current read and write buffers for crypto_channel_continue()
+    union {
+        const char *write_buffer;
+        char *read_buffer;
+    };
+    union {
+        size_t write_total;
+        size_t read_total;
+    };
+    union {
+        size_t write_processed;
+        size_t read_processed;
+    };
+} crypto_channel_t;
+
+
+/**
+ * @brief   Generates the local and remote side of a key.
+ */
+void crypto_generate_keys(local_key_t *local, remote_key_t *remote);
+
+/**
+ * @brief   Prepares a channel for writing and reading by setting its underlying fd and
+ *          key material.
+ */
+void crypto_channel_init(crypto_channel_t *channel, int fd, const local_key_t *local_key, const remote_key_t *remote_key);
+
+/**
+ * @brief   If the channel is blocking, returns 0 when the entire message has been
+ *          sent or -1 if the message cannot be sent. If the channel is nonblocking,
+ *          may return CHANNEL_WRITE_WAIT if the underlying fd must be polled for
+ *          write before calling crypto_channel_continue().
+ * 
+ * @return  0 on success, -1 on error, or CHANNEL_WRITE_WAIT as appropriate if the
+ *          channel is nonblocking.
+ */
+int crypto_channel_write(crypto_channel_t *channel, const void *buffer, size_t bytes);
+
+/**
+ * @brief   If the channel is blocking, returns 0 when the message has been
+ *          received or -1 if an error occured. If the channel is nonblocking,
+ *          may return CHANNEL_READ_WAIT if the underlying fd must be polled for
+ *          read before calling crypto_channel_continue().
+ * 
+ * @return  0 on success, -1 on error, or CHANNEL_READ_WAIT as appropriate if the
+ *          channel is nonblocking.
+ */
+int crypto_channel_read(crypto_channel_t *channel, void *buffer, size_t bytes);
+
+/**
+ * @return  0 on success, -1 on error, CHANNEL_READ_WAIT if the channel's fd must be polled
+ *          for read before calling crypto_channel_continue() again, or CHANNEL_WRITE_WAIT
+ *          if the channel's fd must be polled for writing before calling crypto_channel_continue()
+ *          again.
+ * 
+ * @warning crypto_channel_continue() should only be called on channels with nonblocking fd's,
+ *          after getting CHANNEL_WRITE_WAIT or CHANNEL_READ_WAIT as a return status from a
+*           crypto_channel_write() or crypto_channel_read() call.
+ */
+int crypto_channel_continue(crypto_channel_t *channel);
+
+#endif
---
+++ src/crypto.c
@@ -0,0 +1,271 @@
+#include <endian.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/param.h>
+#include <sys/socket.h>
+
+#include <nacl/randombytes.h>
+#include "opal/crypto.h"
+
+
+// Static functions
+static void crypto_channel_encrypt_chunk(crypto_channel_t *channel)
+{
+    // Zero the necessary prefix bytes
+    bzero(channel->plaintext_zeroes, crypto_box_ZEROBYTES);
+    // Generate a nonce
+    uint8_t nonce[crypto_box_NONCEBYTES];
+    randombytes(nonce, crypto_box_NONCEBYTES);
+    // Encrypt the plaintext, storing it into channel->ciphertext_data
+    crypto_box_afternm(
+        channel->ciphertext_zeroes,
+        channel->plaintext_zeroes,
+        crypto_box_ZEROBYTES + channel->plaintext_length,
+        nonce,
+        channel->key
+    );
+    // Store the ciphertext network length
+    channel->ciphertext_network_length = htobe16(channel->plaintext_length);
+    // Store the ciphertext nonce
+    memcpy(channel->ciphertext_nonce, nonce, crypto_secretbox_NONCEBYTES);
+    // Store ciphertext size and zero processed bytes
+    channel->ciphertext_length = channel->plaintext_length + CRYPTO_HEADER_SIZE;
+    channel->ciphertext_processed = 0;
+}
+
+
+static int crypto_channel_decrypt_chunk(crypto_channel_t *channel)
+{
+    // Retreive the nonce from the buffer
+    uint8_t nonce[crypto_secretbox_NONCEBYTES];
+    memcpy(nonce, channel->ciphertext_nonce, crypto_secretbox_NONCEBYTES);
+    // Zero the necessary prefix bytes (erasing nonce and network length)
+    bzero(channel->ciphertext_zeroes, crypto_secretbox_ZEROBYTES);
+    // Perform decryption, putting data into channel->plaintext
+    int status = crypto_box_open_afternm(
+        channel->plaintext_zeroes,
+        channel->ciphertext_zeroes,
+        crypto_secretbox_ZEROBYTES + channel->ciphertext_length - CRYPTO_HEADER_SIZE,
+        nonce,
+        channel->key
+    );
+    // Store plaintext size and zero processed bytes
+    channel->plaintext_length = channel->ciphertext_length - CRYPTO_HEADER_SIZE;
+    return status;
+}
+
+
+static int crypto_channel_receive_chunk(crypto_channel_t *channel)
+{
+    while (channel->ciphertext_processed < channel->ciphertext_length)
+    {
+        ssize_t bytes_read = read(
+                channel->fd,
+                channel->ciphertext + channel->ciphertext_processed,
+                channel->ciphertext_length - channel->ciphertext_processed);
+        if (bytes_read == 0)
+        {
+            return CHANNEL_ERROR;
+        }
+        else if (bytes_read == -1)
+        {
+            if (errno == EAGAIN)
+            {
+                return CHANNEL_READ_WAIT;
+            }
+            else
+            {
+                return CHANNEL_ERROR;
+            }
+        }
+        else
+        {
+            channel->ciphertext_processed += bytes_read;
+            if (channel->ciphertext_processed > CRYPTO_LENGTH_BYTES)
+            {
+                channel->ciphertext_length = be16toh(channel->ciphertext_network_length) + CRYPTO_HEADER_SIZE;
+            }
+        }
+    }
+
+    return CHANNEL_SUCCESS;
+}
+
+
+static int crypto_channel_send_chunk(crypto_channel_t *channel)
+{
+    while (channel->ciphertext_processed < channel->ciphertext_length)
+    {
+        ssize_t bytes_written = write(
+                channel->fd,
+                channel->ciphertext + channel->ciphertext_processed,
+                channel->ciphertext_length - channel->ciphertext_processed);
+        if (bytes_written == 0)
+        {
+            return CHANNEL_ERROR;
+        }
+        else if (bytes_written == -1)
+        {
+            if (errno == EAGAIN)
+            {
+                return CHANNEL_WRITE_WAIT;
+            }
+            else
+            {
+                return CHANNEL_ERROR;
+            }
+        }
+        else
+        {
+            channel->ciphertext_processed += bytes_written;
+        }
+    }
+
+    return CHANNEL_SUCCESS;
+}
+
+
+static int crypto_channel_write_continue(crypto_channel_t *channel)
+{
+    // Chunk and send the message until an error, write wait, or done
+    while (channel->write_processed < channel->write_total) {
+        // Send the previous chunk
+        int status = crypto_channel_send_chunk(channel);
+        if (status == CHANNEL_ERROR || status == CHANNEL_WRITE_WAIT)
+        {
+            return status;
+        }
+        channel->write_processed += channel->plaintext_length;
+
+        // Determine a size for the next chunk
+        channel->plaintext_length = MAX(channel->write_total - channel->write_processed, CRYPTO_PACKET_MAX);
+        // Move the chunk into the plaintext buffer
+        memcpy(channel->plaintext_data, channel->write_buffer + channel->write_processed, channel->plaintext_length);
+        // Encrypt the chunk
+        crypto_channel_encrypt_chunk(channel);
+    }
+
+    channel->operation = NO_OP;
+    return CHANNEL_SUCCESS;
+}
+
+
+static int crypto_channel_read_continue(crypto_channel_t *channel)
+{
+    int status;
+
+    while (channel->read_processed < channel->read_total)
+    {
+        // Figure out how many bytes are still needed
+        size_t bytes_needed = channel->read_total - channel->read_processed;
+
+        // Consume bytes out of the unread data field if there are any
+        size_t unread_bytes_available = channel->unread_data_end - channel->unread_data_start;
+        if (unread_bytes_available > 0)
+        {
+            size_t unread_bytes_to_consume = MIN(bytes_needed, unread_bytes_available);
+            memcpy(channel->read_buffer, channel->unread_data + channel->unread_data_start, unread_bytes_to_consume);
+            channel->read_processed += unread_bytes_to_consume;
+            channel->unread_data_start += unread_bytes_to_consume;
+            if (channel->read_processed == channel->read_total)
+            {
+                break;
+            }
+        }
+
+        // Receive a chunk if more data is needed
+        status = crypto_channel_receive_chunk(channel);
+        if (status == CHANNEL_ERROR || status == CHANNEL_READ_WAIT)
+        {
+            return status;
+        }
+        // Decrypt the chunk
+        status = crypto_channel_decrypt_chunk(channel);
+        if (status == -1)
+        {
+            return CHANNEL_ERROR;
+        }
+
+        // Calculate the number of bytes from this chunk to consume
+        size_t chunk_bytes_available = channel->plaintext_length;
+        size_t chunk_bytes_to_consume = MIN(bytes_needed, chunk_bytes_available);
+        size_t chunk_bytes_remaining = chunk_bytes_available - chunk_bytes_to_consume;
+        // Move the consumed chunk data into the read buffer
+        memcpy(channel->read_buffer, channel->plaintext, chunk_bytes_to_consume);
+        channel->read_processed += chunk_bytes_to_consume;
+        // Store the rest of the chunk data in the unread data field
+        if (chunk_bytes_remaining > 0)
+        {
+            memcpy(channel->unread_data, channel->plaintext + chunk_bytes_to_consume, chunk_bytes_remaining);
+        }
+        channel->unread_data_start = 0;
+        channel->unread_data_end = chunk_bytes_remaining;
+    }
+
+    channel->operation = NO_OP;
+    return CHANNEL_SUCCESS;
+}
+
+
+// Public functions
+void crypto_generate_keys(remote_key_t *remote, local_key_t *local)
+{
+    crypto_box_keypair(remote, local);
+}
+
+
+void crypto_channel_init(crypto_channel_t *channel, int fd, const local_key_t *local_key, const remote_key_t *remote_key)
+{
+    channel->fd = fd;
+    channel->operation = NO_OP;
+    channel->unread_data_start = 0;
+    channel->unread_data_end = 0;
+    crypto_box_beforenm(channel->key, remote_key, local_key);
+}
+
+
+int crypto_channel_write(crypto_channel_t *channel, const void *buffer, size_t bytes)
+{
+    // Prepare for writing
+    channel->write_buffer = buffer;
+    channel->write_total = bytes;
+    channel->write_processed = 0;
+    channel->ciphertext_processed = 0;
+    channel->ciphertext_length = 0;
+    channel->plaintext_length = 0;
+    channel->operation = WRITE_OP;
+    // Execute writing operation
+    return crypto_channel_write_continue(channel);
+}
+
+
+int crypto_channel_read(crypto_channel_t *channel, void *buffer, size_t bytes)
+{
+    // Prepare for reading
+    channel->read_buffer = buffer;
+    channel->read_total = bytes;
+    channel->read_processed = 0;
+    channel->ciphertext_processed = 0;
+    channel->ciphertext_length = CRYPTO_HEADER_SIZE;
+    channel->plaintext_length = 0;
+    channel->operation = READ_OP;
+    // Execute reading operation
+    return crypto_channel_read_continue(channel);
+}
+
+
+int crypto_channel_continue(crypto_channel_t *channel)
+{
+    switch (channel->operation)
+    {
+        case READ_OP:
+            return crypto_channel_read_continue(channel);
+        case WRITE_OP:
+            return crypto_channel_write_continue(channel);
+        case NO_OP:
+            return CHANNEL_SUCCESS;
+        default:
+            return CHANNEL_ERROR;
+    }
+}
